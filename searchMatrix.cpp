9453555497835e01a86d7485b80f8d02d5ff5b3b
onkar <onkar.chendage@gmail.com>
2023-06-05 21:44:50 +0530

    add Search in 2D matrix

---
+++ searchMatrix.cpp
@@ -0,0 +1,24 @@
+/*
+
+https://www.codingninjas.com/codestudio/problems/search-in-a-2d-matrix_8230773?challengeSlug=striver-sde-challenge
+
+*/
+
+bool search(vector<int>&arr, int k) {
+    int l=0, r=arr.size()-1;
+    while (l<=r) {
+        int mid = l + (r-l)/2;
+        if (arr[mid]==k) return true;
+        if (arr[mid]<k) l = mid+1;
+        else r = mid-1;
+    }
+    return false;
+}
+
+bool searchMatrix(vector<vector<int>>& mat, int target) {
+    for (auto& row : mat) {
+        if (search(row, target)) return true;
+    }
+
+    return false;
+}
