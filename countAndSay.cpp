d7c49f8786c613005fc144e6147f6deb7ea2c011
onkar <onkar.chendage@gmail.com>
2023-07-14 10:00:45 +0530

    add new problems

---
+++ countAndSay.cpp
@@ -0,0 +1,27 @@
+/*
+
+Problem Link: https://www.codingninjas.com/studio/problems/count-and-say_8230807?challengeSlug=striver-sde-challenge
+
+*/
+
+#include <bits/stdc++.h> 
+string writeAsYouSpeak(int n) 
+{
+	// Write your code here.	
+	string s="1";
+    for(int i=0;i<n-1;i++) {
+        string a;
+        for(int j=0;j<s.length();) {
+            int cnt=0;
+            int k=j;
+            while (j < s.length() && s[j] == s[k]) {
+              cnt++;
+              k++;
+            }
+            a += to_string(cnt) + s[j];
+            j = k;
+        }
+        s = a;
+    }
+    return s;
+}
